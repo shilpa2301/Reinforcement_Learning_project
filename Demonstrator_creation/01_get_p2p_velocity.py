a73148325c1b23f4bffd28c3c2a9f1c1bd27a668
ChunkaiFu <chunkai369@gmail.com>
2023-09-25 12:02:17 -0500

    processed the interpolated data

--- Demonstrator_creation/01_get_p2p_velocity.py
+++ Demonstrator_creation/01_get_p2p_velocity.py
@@ -14,6 +14,7 @@ def calculate_velocity(df):
 
     return velocities
 
+
 def calculate_omega(df):
     omegas = []
     for i in range(1, len(df)):
@@ -27,23 +28,33 @@ def calculate_omega(df):
     return omegas
 
 
+def printlines(list, start_line, end_line):
+    for i, item in enumerate(list):
+        if end_line > i > start_line:
+            print(item)
+
 import pandas as pd
 import os
 
-file_path = os.path.join('Data', 'First_Set_for_imitation_Learning', 'data_8_clockwise', 'vehicle_waypoint.csv')
-track_file = pd.read_csv(file_path)
-print(track_file.columns)
+print(os.getcwd())
+dir_path = os.path.join('Data', 'First_Set_for_imitation_Learning', 'interpolated_8_clockwise')
+result_path = os.path.join('Data', 'First_Set_for_imitation_Learning', 'results')
+for file in os.listdir(dir_path):
+    file_path = os.path.join(os.getcwd(), dir_path, file)
+    print(file_path)
+    track_file = pd.read_csv(file_path)
+    print(track_file.columns)
+    velocities = calculate_velocity(track_file)
+    omegas = calculate_omega(track_file)
 
-velocities = calculate_velocity(track_file)
-omegas = calculate_omega(track_file)
+    print("Velocity: ")
+    printlines(velocities, 0, 20)
 
-print("Velocity: ")
-for i, velocity in enumerate(velocities):
-    if i < 20:
-        print(velocity)
+    print("Omega: ")
+    printlines(omegas, 0, 20)
 
-print("Omega: ")
-for i, omega in enumerate(omegas):
-    if i < 20:
-        print(omega)
+    # write out the results here
+
+
+
 
